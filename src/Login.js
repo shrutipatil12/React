d68468527cd9978ad9ad4bfce116c78b7061e235
shrutidhananjaypatil <shrutidpatil12@gmail.com>
2020-06-17 17:41:06 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+
+return <div>
+    <form className="App">
+    <label htmlFor="Enter your Email">Email</label><br></br>
+    <input name="Enter your Email" type="text" placeholder="Enter your email" /><br></br>
+    <button type="submit">
+            Request for Login
+    </button>
+    </form>
+    </div>  
+
+}
+
+export default Login;
